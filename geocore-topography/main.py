55c3f7a3159ec128b4c26be8e265c9f9893b3ae5
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-07 22:27:57 +0530

    Add geocore-topography boilerplate
    - Added a Dockerfile
    - Added a main.py app script
    - Added a requirements.txt
    - Added a README

---
+++ geocore-topography/main.py
@@ -0,0 +1,76 @@
+"""
+GeoSentry GeoCore API
+
+Google Cloud Platform - Cloud Run
+
+geocore-topography service
+"""
+import os
+import json
+import flask
+import flask_restful
+
+class RunLogger:
+    """ Class that builds serverless log compliant with Google Cloud Platform """
+
+    service = "geocore-topography"
+
+    def __init__(self, workflow: str) -> None:
+        """ Initialization Method """
+        self.workflow: str = workflow
+        self.logtrace: list = [f"execution started. worflow - {workflow}."]
+
+        self.baselog = {
+            "trace": self.logtrace, 
+            "workflow": self.workflow, 
+            "service":self.service, 
+            "logging.googleapis.com/trace": flask.request.headers.get('X-Cloud-Trace-Context')
+        }
+
+    def addtrace(self, trace: str):
+        """ A method that adds a trace string to the list of logtraces. """
+        self.logtrace.append(trace)
+
+    def flush(self, severity: str, message: str):
+        """
+        A method that creates and flushes the built log to Cloud Logging as a structured 
+        log given that it is called within a Cloud Run/Functions Service. 
+        The method accepts a log severity string and a log message string.
+
+        Accepted log severity values are - EMERGENCY, ALERT, CRITICAL, ERROR, WARNING, NOTICE, INFO, DEBUG and DEFAULT.
+        Refer to https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#logseverity for more information.
+        """
+        logentry = dict(severity=severity, message=message)
+        logentry.update(self.baselog)
+        print(json.dumps(logentry))
+
+class SCL(flask_restful.Resource):
+
+    def post(self):
+        """ The runtime for when the '/scl' endpoint recieves a POST request """
+        logger = RunLogger("scl")
+
+        request = flask.request.get_json()
+        logger.flush("INFO", f"{request}")
+
+        return f"complete", 200
+
+class Elevation(flask_restful.Resource):
+
+    def post(self):
+        """ The runtime for when the '/elevation' endpoint recieves a POST request """
+        logger = RunLogger("elevation")
+
+        request = flask.request.get_json()
+        logger.flush("INFO", f"{request}")
+
+        return f"complete", 200
+
+app = flask.Flask(__name__)
+api = flask_restful.Api(app)
+
+api.add_resource(SCL, '/scl')
+api.add_resource(Elevation, '/elevation')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
