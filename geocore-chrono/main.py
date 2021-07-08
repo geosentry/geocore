247f4dff89c7890505563dc1217093c64a201f1d
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-08 21:28:48 +0530

    Rename geocore-lite
    - Renamed geocore-lite to geocore-chrono
    - Removed the /reshape endpoint

--- geocore-chrono/main.py
+++ geocore-chrono/main.py
@@ -3,17 +3,17 @@ GeoSentry GeoCore API
 
 Google Cloud Platform - Cloud Run
 
-geocore-lite service
+geocore-chrono service
 """
 import os
 import json
 import flask
 import flask_restful
 
-class RunLogger:
-    """ Class that builds serverless log compliant with Google Cloud Platform """
+class LogEntry:
+    """ A class that represents a serverless log compliant with Google Cloud Platform. """
 
-    service = "geocore-lite"
+    service = "geocore-chrono"
 
     def __init__(self, workflow: str) -> None:
         """ Initialization Method """
@@ -22,8 +22,7 @@ class RunLogger:
 
         self.baselog = {
             "trace": self.logtrace, 
-            "workflow": self.workflow, 
-            "service":self.service, 
+            "service":self.service,
             "logging.googleapis.com/trace": flask.request.headers.get('X-Cloud-Trace-Context')
         }
 
@@ -33,22 +32,26 @@ class RunLogger:
 
     def flush(self, severity: str, message: str):
         """
-        A method that creates and flushes the built log to Cloud Logging as a structured 
-        log given that it is called within a Cloud Run/Functions Service. 
+        A method of LogEntry that creates and flushes the built log to Cloud Logging as
+        a structured log given that it is called within a Cloud Run/Functions Service.
         The method accepts a log severity string and a log message string.
-
         Accepted log severity values are - EMERGENCY, ALERT, CRITICAL, ERROR, WARNING, NOTICE, INFO, DEBUG and DEFAULT.
         Refer to https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#logseverity for more information.
         """
+        import json
+
+        self.addtrace("execution ended.")
         logentry = dict(severity=severity, message=message)
         logentry.update(self.baselog)
+
         print(json.dumps(logentry))
+
 
 class Check(flask_restful.Resource):
 
     def post(self):
         """ The runtime for when the '/check' endpoint recieves a POST request """
-        logger = RunLogger("check")
+        logger = LogEntry("check")
 
         request = flask.request.get_json()
         logger.flush("INFO", f"{request}")
@@ -59,30 +62,19 @@ class Select(flask_restful.Resource):
 
     def post(self):
         """ The runtime for when the '/select' endpoint recieves a POST request """
-        logger = RunLogger("select")
+        logger = LogEntry("select")
 
         request = flask.request.get_json()
         logger.flush("INFO", f"{request}")
 
         return f"complete", 200
 
-class Reshape(flask_restful.Resource):
-
-    def post(self):
-        """ The runtime for when the '/reshape' endpoint recieves a POST request """
-        logger = RunLogger("reshape")
-
-        request = flask.request.get_json()
-        logger.flush("INFO", f"{request}")
-
-        return f"complete", 200
 
 app = flask.Flask(__name__)
 api = flask_restful.Api(app)
 
 api.add_resource(Check, '/check')
 api.add_resource(Select, '/select')
-api.add_resource(Reshape, '/reshape')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
